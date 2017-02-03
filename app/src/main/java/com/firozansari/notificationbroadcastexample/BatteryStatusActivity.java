e1eb1d4d0161cfb81857cdf6753b29753b7d91a9
Firoz Ansari <firoz.ansari@topcashback.co.uk>
2017-02-03 08:09:09 +0000

    description added

--- app/src/main/java/com/firozansari/notificationbroadcastexample/BatteryStatusActivity.java
+++ app/src/main/java/com/firozansari/notificationbroadcastexample/BatteryStatusActivity.java
@@ -36,22 +36,34 @@ public class BatteryStatusActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
 
-        IntentFilter f=new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        IntentFilter intentFilter =new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
 
-        registerReceiver(onBattery, f);
+        registerReceiver(onBattery, intentFilter);
     }
 
     @Override
     public void onPause() {
         unregisterReceiver(onBattery);
-
         super.onPause();
     }
 
     BroadcastReceiver onBattery=new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
-            int pct=
-                    100 * intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 1)
+
+            //Following constants defined on the BatteryManager class:
+            //EXTRA_HEALTH, which should generally be BATTERY_HEALTH_GOOD
+            //EXTRA_LEVEL, which is the proportion of battery life remaining as an integer, specified on the scale described by the EXTRA_SCALE value
+            //EXTRA_PLUGGED, which will indicate if the device is plugged into AC power (BATTERY_PLUGGED_AC) or USB power (BATTERY_PLUGGED_USB)
+            //EXTRA_SCALE, which indicates the maximum possible value of level (e.g., 100, indicating that level is a percentage of charge remaining)
+            //EXTRA_STATUS, which will tell you if the battery is charging (BATTERY_STATUS_CHARGING), full (BATTERY_STATUS_FULL), or discharging (BATTERY_STATUS_DISCHARGING)
+            //EXTRA_TECHNOLOGY, which indicates what sort of battery is installed (e.g., "Li-Ion")
+            //EXTRA_TEMPERATURE, which tells you how warm the battery is, in tenths of a degree Celsius (e.g., 213 is 21.3 degrees Celsius)
+            //EXTRA_VOLTAGE, indicating the current voltage being delivered by the battery, in millivolts
+
+            //To get current battery information on Android 5.0 and higher, BatteryManager offers getIntProperty() and getLongProperty(), where the keys for the “properties”
+            // are BATTERY_PROPERTY_* constants defined on BatteryManager, such as BATTERY_PROPERTY_CAPACITY to determine the percentage of remaining battery capacity.
+
+            int pct= 100 * intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 1)
                             / intent.getIntExtra(BatteryManager.EXTRA_SCALE, 1);
 
             bar.setProgress(pct);
