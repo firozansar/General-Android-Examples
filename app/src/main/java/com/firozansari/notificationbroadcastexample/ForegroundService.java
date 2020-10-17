abf3e0813814be73f1892ccbef1cd95c8a43a0a8
Firoz Ansari <firozansari@stagecoach-technology.com>
2020-10-17 07:08:37 +0100

    Updated to AndroidX

--- app/src/main/java/com/firozansari/notificationbroadcastexample/ForegroundService.java
+++ app/src/main/java/com/firozansari/notificationbroadcastexample/ForegroundService.java
@@ -7,10 +7,8 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
--- app/src/main/java/com/firozansari/notificationbroadcastexample/SimpleWakefulReceiver.java
+++ app/src/main/java/com/firozansari/notificationbroadcastexample/SimpleWakefulReceiver.java
@@ -3,7 +3,7 @@ package com.firozansari.notificationbroadcastexample;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
-import android.support.v4.content.WakefulBroadcastReceiver;
+import androidx.legacy.content.WakefulBroadcastReceiver;
 import android.util.Log;
 
 /**
