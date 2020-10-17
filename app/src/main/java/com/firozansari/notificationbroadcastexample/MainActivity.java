2f8eb2b30273222ed78ced0217643662c36b4030
Firoz Ansari <firozansari@stagecoach-technology.com>
2020-10-17 07:13:36 +0100

    UI changes

--- app/src/main/java/com/firozansari/notificationbroadcastexample/MainActivity.java
+++ app/src/main/java/com/firozansari/notificationbroadcastexample/MainActivity.java
@@ -73,16 +73,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Always cast your custom Toolbar here, and set it as the ActionBar.
-        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(tb);
+        //Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(tb);
 
         // Get the ActionBar here to configure the way it behaves.
-        final ActionBar ab = getSupportActionBar();
+        //final ActionBar ab = getSupportActionBar();
         //ab.setHomeAsUpIndicator(R.drawable.ic_menu); // set a custom icon for the default home button
         //ab.setDisplayShowHomeEnabled(true); // show or hide the default home button
-        ab.setDisplayHomeAsUpEnabled(true);
-        ab.setDisplayShowCustomEnabled(true); // enable overriding the default toolbar layout
-        ab.setDisplayShowTitleEnabled(false); // disable the default title element here (for centered title)
+        //ab.setDisplayHomeAsUpEnabled(true);
+        //ab.setDisplayShowCustomEnabled(true); // enable overriding the default toolbar layout
+        //ab.setDisplayShowTitleEnabled(false); // disable the default title element here (for centered title)
 
 
         // Initialize buttons
